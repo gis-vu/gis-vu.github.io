c7527cb53ad0873441ca006f187d021aa7995436
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-09-30 14:26:59 +0300

    WIP

--- site.js
+++ site.js
@@ -22,7 +22,8 @@ map.on('load', function() {
         "layout": {},
         "paint": {
             "fill-color": "#228B22",
-            "fill-opacity": 1
+            "fill-opacity": 1,
+            "fill-outline-color": "black"
         }
     });
 
@@ -48,5 +49,67 @@ map.on('load', function() {
         },
         "interactive": false
     });
-
 });
+
+ map.on('mouseenter', 'ltu-parks', function () {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', 'ltu-parks', function () {
+        map.getCanvas().style.cursor = '';
+    });
+
+
+ map.on('click', 'ltu-parks', function (e) {
+        var coordinates = e.features[0].geometry.coordinates;
+        var name = e.features[0].properties.name;
+    
+        var bounds = getBoundingBox(e);
+
+        map.fitBounds(bounds, {
+            padding: 20
+        });
+     
+//     alert(name);
+ })
+
+function getBoundingBox(data) {
+    var bounds = {};
+
+    var coordinates = data.features[0].geometry.coordinates;
+    
+    var coords = [];
+    
+     for (var i = 0; i < coordinates.length; i++) {
+         coords= coords.concat(getElements(coordinates[i]));         
+     }
+ 
+    for (var j = 0; j <coords.length; j++) {
+      var longitude = coords[j][0];
+      var latitude = coords[j][1];
+      bounds.xMin = bounds.xMin < longitude ? bounds.xMin : longitude;
+      bounds.xMax = bounds.xMax > longitude ? bounds.xMax : longitude;
+      bounds.yMin = bounds.yMin < latitude ? bounds.yMin : latitude;
+      bounds.yMax = bounds.yMax > latitude ? bounds.yMax : latitude;
+    }
+  
+
+  return [[bounds.xMax, bounds.yMax],[bounds.xMin, bounds.yMin]];
+}
+
+function getElements(data){
+    
+    if(Array.isArray(data) && Array.isArray(data[0]) && !Array.isArray(data[0][0]))
+        return data;
+    
+    var coords = [];
+    
+    for (var i = 0; i < data.length; i++) {
+        coords= coords.concat(getElements(data[i]));         
+    }
+    
+    return coords;
+}
+    
+
