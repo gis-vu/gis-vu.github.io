f7656adb81189396fa175222ea810a758dab3680
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-12-11 15:53:03 +0200

    WIP load request

--- settingsFunctions.js
+++ settingsFunctions.js
@@ -21,13 +21,60 @@ function invertRequestWindow(){
 
 function requestWindowOpen(){
     invertRequestWindow();
+        
+    $("#requestTextBox").val(JSON.stringify(getRequestData(), null, 4));  
+}
+
+function revertRequest(){
     
-//    alert(JSON.stringify(getRequestData(), null, 4));
+    try {
+        var request = JSON.parse($("#requestTextBox").val());
+        revertRequestHelper(request);
+    }
+    catch(err) {
+      alert(err);
+    }    
+}
+
+
+function revertRequestHelper(request){
     
-    $("#requestTextBox").text(JSON.stringify(getRequestData(), null, 4));
+    var startPoint = request.start;
+    var endPoint = request.end;
+    var polygonPoints = request.polygonPoints;
+    var points = request.points;
+   
+    
+    var routeOverlapValue = request.searchOptions.trackOverlapImportance;
+    var forrestValue = request.searchOptions.propertyValueImportance[0].importance;
+    var waterValue = request.searchOptions.propertyValueImportance[1].importance;
+    var waterDistanceValue = request.searchOptions.propertyValueImportance[1].threshold;
+    
+    var pathValue = Number(request.searchOptions.propertyImportance[0].importance);
+    var walkingPathValue = Number(request.searchOptions.propertyImportance[3].importance);
+    var routeValue = Number(request.searchOptions.propertyImportance[5].importance);
+    var seriousRouteValue = Number(request.searchOptions.propertyImportance[17].importance);
+    
+    
+    if(isNaN(pathValue) || isNaN(walkingPathValue) || isNaN(routeValue) || isNaN(seriousRouteValue))
+        throw "Kelio tipo koeficientai įvesti klaidingai";
+
+    $('#pathValue').val(pathValue);
+    $('#walkingPathValue').val(walkingPathValue);
+    $('#routeValue').val(routeValue);
+    $('#seriousRouteValue').val(seriousRouteValue);
+    
+
+//    var routeOverlapValue = $('#routeOverlapValue')[0].value;
+//    
+//    var forrestValue = $('#forrestValue')[0].value;
+//    var waterDistanceValue = $('#waterDistanceValue')[0].value;
+//    var waterValue = $('#waterValue')[0].value;
+
     
     
 }
+
 
 function downloadRouteIndex(index){
          
